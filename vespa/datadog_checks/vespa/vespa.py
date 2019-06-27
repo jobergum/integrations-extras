6614d1918c091e994978a2914fe68756d1ce9d3b
gjoranv <gv@verizonmedia.com>
2019-06-27 02:49:27 +0200

    Implement parsing of metricsproxy output, with tests.

--- vespa/datadog_checks/vespa/vespa.py
+++ vespa/datadog_checks/vespa/vespa.py
@@ -12,10 +12,11 @@ class VespaCheck(AgentCheck):
 
     VESPA_SERVICE_CHECK = 'vespa.health'
     URL = 'http://localhost:19092/metrics/v1/values'
-    count = 0
+    metric_count = 0
+    services_up = 0
 
     def check(self, instance):
-        self.count = 0
+        self.metric_count = 0
 
         instance_tags = instance.get('tags', [])
         consumer = instance.get('consumer')
@@ -23,52 +24,51 @@ class VespaCheck(AgentCheck):
             raise CheckException("Configuration error - no consumer defined")
         url = self.URL + '?consumer=' + consumer
         try:
-            json = self._get_state_metrics(url, 10.0, instance_tags)
-            status = json["status"]["code"]
-            if status == "up":
-                self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.OK, tags=instance_tags,
-                                   message="Service returns up")
-            elif status == "down":
-                self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.WARNING, tags=instance_tags,
-                                   message="Service reports down")
-            else:
-                self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.WARNING, tags=instance_tags,
-                                   message="Service reports unknown status")
-            self._emit_metrics(json, instance_tags)
-            self.log.info('Forwarded %s metrics to hq ' % self.count)
+            json = self._get_metrics_json(url, 10.0, instance_tags)
+            for service in json['services']:
+                service_name = service['name']
+                self._report_service_status(instance_tags, service_name, service)
+                for metrics in service['metrics']:
+                    self._emit_metrics(service_name, metrics, instance_tags)
+            self.log.info("Forwarded {} metrics to hq for {} services".format(self.metric_count, self.services_up))
         except Exception as e:
             self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.WARNING, tags=instance_tags,
                                message="Exception {} ".format(e))
 
-    def _emit_metrics(self, json, instance_tags):
-        if 'metrics' not in json:
+    def _emit_metrics(self, service_name, json, instance_tags):
+        """ Emit one metrics packet, which consists of a set of metrics that share the same set of dimensions.
+        :param json: A json object with 'values' and 'dimensions'
+        """
+        if 'values' not in json:
             return
-        for metric in json['metrics']['values']:
-            name = metric['name']
-            values = metric['values']
-            dimensions = dict()
-            if 'dimensions' in metric:
-                dimensions = metric['dimensions']
-            logging.debug('metric: %s, dimensions: %s', name, dimensions)
-            self._emit_metric(name, values, instance_tags, dimensions)
+        dimensions = dict()
+        if 'dimensions' in json:
+            dimensions = json['dimensions']
+        for name, value in json['values'].items():
+            full_name = service_name + '.' + name
+            self._emit_metric(full_name, value, instance_tags, dimensions)
 
-    def _emit_metric(self, name, values, instance_tags, dimensions):
+    def _emit_metric(self, name, value, instance_tags, dimensions):
         tags = []
-        for k in dimensions.keys():
-            tags.append(k + ":" + dimensions[k])
+        for dim, dim_val in dimensions.items():
+            tags.append(dim + ":" + dim_val)
         instance_tags = tags + instance_tags
-        self.gauge(name, values['rate'], instance_tags)
-        self.count += 1
+        logging.debug("metric: {}, dimensions: {}".format(name, instance_tags))
+        self.gauge(name, value, instance_tags)
+        self.metric_count += 1
 
-    def _get_state_metrics(self, url, timeout, instance_tags):
+    def _get_metrics_json(self, url, timeout, instance_tags):
+        """ Send rest request to metrics api and return the response as JSON
         """
-        Send rest request to state api and return the response as JSON
-        """
-        self.log.info('Sending request to "%s"' % url)
+        self.log.info("Sending request to {}".format(url))
         try:
             response = requests.get(url, timeout=timeout)
             response.raise_for_status()
             response = response.json()
+            if 'services' not in response:
+                self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.CRITICAL, tags=instance_tags,
+                                   message="No services in response from metrics proxy on {}".format(url))
+                raise
 
         except Timeout as e:
             self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.CRITICAL, tags=instance_tags,
@@ -86,3 +86,19 @@ class VespaCheck(AgentCheck):
             raise
 
         return response
+
+    def _report_service_status(self, instance_tags, service_name, service):
+        code = service["status"]["code"]
+        description = service["status"]["description"]
+        if code == "up":
+            self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.OK, tags=instance_tags,
+                               message="Service {} returns up".format(service_name))
+            self.services_up += 1
+        elif code == "down":
+            self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.WARNING, tags=instance_tags,
+                               message="Service {} reports down: {}".format(service_name, description))
+            self.log.warn("Service {} reports down: {}".format(service_name, description))
+        else:
+            self.service_check(self.VESPA_SERVICE_CHECK, AgentCheck.WARNING, tags=instance_tags,
+                               message="Service {} reports unknown status: {}".format(service_name, description))
+            self.log.warn("Service {} reports unknown status: {}".format(service_name, description))
--- vespa/tests/test_vespa.py
+++ vespa/tests/test_vespa.py
@@ -8,7 +8,7 @@ from mock import MagicMock
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_no_config_raises():
+def test_no_consumer_raises():
     check = VespaCheck("vespa", {}, {})
     with pytest.raises(CheckException):
         check.check({})
@@ -24,32 +24,54 @@ def test_cannot_connect_is_critical(aggregator):
 
 def test_service_reports_down(aggregator):
     check = VespaCheck("vespa", {}, {})
-    with open(os.path.join(HERE, 'node_down.json'), 'r') as f:
-        check._get_state_metrics = MagicMock(return_value=json.load(f))
+    with open(os.path.join(HERE, 'service_down.json'), 'r') as f:
+        check._get_metrics_json = MagicMock(return_value=json.load(f))
     check.check({'consumer': 'default'})
     aggregator.assert_service_check(check.VESPA_SERVICE_CHECK,
                                     VespaCheck.WARNING,
-                                    message="Service reports down")
+                                    message='Service vespa.down-service reports down: No response',
+                                    count=1)
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK, VespaCheck.OK, count=0)
 
 
 def test_service_reports_unknown(aggregator):
     check = VespaCheck("vespa", {}, {})
-    with open(os.path.join(HERE, 'node_unknown.json'), 'r') as f:
-        check._get_state_metrics = MagicMock(return_value=json.load(f))
+    with open(os.path.join(HERE, 'service_unknown.json'), 'r') as f:
+        check._get_metrics_json = MagicMock(return_value=json.load(f))
     check.check({'consumer': 'default'})
     aggregator.assert_service_check(check.VESPA_SERVICE_CHECK,
                                     VespaCheck.WARNING,
-                                    message="Service reports unknown status")
+                                    message='Service vespa.unknown-service reports unknown status: Empty status page',
+                                    count=1)
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK, VespaCheck.OK, count=0)
+
+
+def test_down_service_does_not_raise(aggregator):
+    check = VespaCheck("vespa", {}, {})
+    with open(os.path.join(HERE, 'service_up_and_down.json'), 'r') as f:
+        check._get_metrics_json = MagicMock(return_value=json.load(f))
+    check.check({'consumer': 'default'})
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK,
+                                    VespaCheck.WARNING,
+                                    message='Service vespa.down-service reports down: No response',
+                                    count=1)
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK,
+                                    VespaCheck.OK,
+                                    message='Service vespa.up-service returns up',
+                                    count=1)
+    assert 3 == check.metric_count
 
 
 def test_check_metrics(aggregator):
     check = VespaCheck("vespa", {}, {})
-    with open(os.path.join(HERE, 'large_content_metrics.json'), 'r') as f:
-        check._get_state_metrics = MagicMock(return_value=json.load(f))
+    with open(os.path.join(HERE, 'metrics_all.json'), 'r') as f:
+        check._get_metrics_json = MagicMock(return_value=json.load(f))
 
     check.check({'consumer': 'default'})
-    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK, VespaCheck.OK)
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK, VespaCheck.OK, count=7)
+    aggregator.assert_service_check(check.VESPA_SERVICE_CHECK, VespaCheck.WARNING, count=0)
 
-    aggregator.assert_metric("content.proton.resource_usage.disk",
-                             value=0.216666, tags=['documenttype:ad', 'cluster:search'])
-    assert 42 == check.count
+    aggregator.assert_metric("vespa.container.http.status.2xx.rate",
+                             value=10, tags=['metrictype:standard', 'instance:container', 'scheme:http',
+                                             'httpMethod:GET', 'clustername:default', 'vespaVersion:7.0.0'])
+    assert 38 == check.metric_count
